77821e65dd845c7a0a177bcd5e824c63aca39b9b
vornicx <vornicx@gmail.com>
2026-08-20 01:46:59 +0200

    Load editorial showroom styling globally

--- src/js/site.js
+++ src/js/site.js
@@ -1,3 +1,8 @@
+const catalogueStyles = document.createElement('link');
+catalogueStyles.rel = 'stylesheet';
+catalogueStyles.href = '/catalog-experience.css';
+document.head.appendChild(catalogueStyles);
+
 await import('./app.js');
 await import('./enhancements.js');
 await import('./catalog-experience.js');
