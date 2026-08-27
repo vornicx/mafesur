a3bd8a08865288cfe9877286d4ea182b377baca5
vornicx <vornicx@gmail.com>
2026-08-27 15:56:13 +0200

    Enable Mafesur Precision v3 across the site

--- src/js/site.js
+++ src/js/site.js
@@ -3,6 +3,12 @@ catalogueStyles.rel = 'stylesheet';
 catalogueStyles.href = '/catalog-experience.css';
 document.head.appendChild(catalogueStyles);
 
+const precisionStyles = document.createElement('link');
+precisionStyles.rel = 'stylesheet';
+precisionStyles.href = '/precision-v3.css';
+document.head.appendChild(precisionStyles);
+
 await import('./app.js');
 await import('./enhancements.js');
 await import('./catalog-experience.js');
+await import('./precision-v3.js');
